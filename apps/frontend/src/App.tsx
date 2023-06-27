e765b6038e8b3cbdda5bd2a4028ba6bcfef16f2e
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2023-06-27 12:18:48 +0400

    Init

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {HelloWorld} from "@react-vite-library/ui/dist/HelloWorld"
 
 function App() {
   return (
     <div className="App">
+      <HelloWorld />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
